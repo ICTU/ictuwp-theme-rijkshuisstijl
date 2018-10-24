b039f448917bfd7baa1a55ffeb2142e694e2c96c
Paul van Buuren <paul@wbvb.nl>
2018-10-24 13:57:35 +0200

    1.2.18 - Grid CSS voor IE11.

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
 // * @author  Paul van Buuren
 // * @license GPL-2.0+
 // * @package wp-rijkshuisstijl
-// * @version 1.2.18f
+// * @version 1.2.18
 // * @desc.   Grid CSS voor IE11.
 // * @link    https://github.com/ICTU/digitale-overheid-wordpress-theme-rijkshuisstijl
  */
@@ -23,7 +23,7 @@ include_once( get_template_directory() . '/lib/init.php' );
 // Constants
 define( 'CHILD_THEME_NAME',                 "Rijkshuisstijl (Digitale Overheid)" );
 define( 'CHILD_THEME_URL',                  "https://wbvb.nl/themes/wp-rijkshuisstijl" );
-define( 'CHILD_THEME_VERSION',              "1.2.18f" );
+define( 'CHILD_THEME_VERSION',              "1.2.18" );
 define( 'CHILD_THEME_VERSION_DESCRIPTION',  "Grid CSS voor IE11." );
 define( 'SHOW_CSS_DEBUG',                   false );
 //define( 'SHOW_CSS_DEBUG',                   true );
