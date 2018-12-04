e7b92fec07f85c717b1c0f0a51c1c9890c4c6bbc
Paul van Buuren <paul@wbvb.nl>
2018-12-04 09:51:25 +0100

    functies voor searchwp hernoemd en opgeschoond

--- includes/search-helper-functions.php
+++ includes/search-helper-functions.php
@@ -18,39 +18,41 @@
 
 //========================================================================================================
 
-function searchwp_term_highlight_auto_excerpt( $excerpt ) {
+function rhswp_searchwp_term_highlight_auto_excerpt( $excerpt ) {
 	global $post;
 	
 	if ( ! is_search() ) {
 		return $excerpt;
 	}
 	// prevent recursion
-	remove_filter( 'get_the_excerpt', 'searchwp_term_highlight_auto_excerpt' );
+	remove_filter( 'get_the_excerpt', 'rhswp_searchwp_term_highlight_auto_excerpt' );
 	
 	$global_excerpt = searchwp_term_highlight_get_the_excerpt_global( $post->ID, null, get_search_query() );
 
-	add_filter( 'get_the_excerpt', 'searchwp_term_highlight_auto_excerpt' );
+	add_filter( 'get_the_excerpt', 'rhswp_searchwp_term_highlight_auto_excerpt' );
 
 	return 'ik ben gefilterd, heur! ' . $global_excerpt;
 
 }
 
-//add_filter( 'get_the_excerpt', 'searchwp_term_highlight_auto_excerpt' );
+//add_filter( 'get_the_excerpt', 'rhswp_searchwp_term_highlight_auto_excerpt' );
 
 //========================================================================================================
 
-function my_maybe_include_term_archive( $include, $engine, $terms ) {
+function rhswp_searchwp_maybe_include_term_archive( $include, $engine, $terms ) {
   // only have term archives included for supplemental search engine with name 'supplemental'
   return ( $engine == 'supplemental' ) ? true : false;
 }
 
-add_filter( 'searchwp_term_archive_enabled', 'my_maybe_include_term_archive', 10, 3 );
+add_filter( 'searchwp_term_archive_enabled', 'rhswp_searchwp_maybe_include_term_archive', 10, 3 );
 
 //========================================================================================================
 
-// exclude anything to do with Forum ID 133643 from search results in SearchWP
-function my_searchwp_exclude( $ids, $engine, $terms ) {
+function rhswp_searchwp_exclude( $ids, $engine, $terms ) {
 
+  $ids[] = 377; // is ID voor dossier 'NL Digibeter'
+
+  // dit was een oud, inmiddels buiten gebruik gesteld, paginatemplate dat de zoekresultaten vervuilde
 	$entries_to_exclude = get_posts(
 		array(
 			'nopaging'    => true,
@@ -64,17 +66,13 @@ function my_searchwp_exclude( $ids, $engine, $terms ) {
       )
 		)
 	);
-	
-  dovardump( $entries_to_exclude, 'my_searchwp_exclude');
 
-	die();
-	
 	$ids = array_unique( array_merge( $ids, array_map( 'absint', $entries_to_exclude ) ) );
+
 	return $ids;
 
 }
 
-// add_filter( 'searchwp_exclude', 'my_searchwp_exclude', 10, 3 );
+add_filter( 'searchwp_exclude', 'rhswp_searchwp_exclude', 10, 3 );
 
 //========================================================================================================
-
