47862549fbaaeabda60c30f8fadad3a4f78352a6
Paul van Buuren <paul@wbvb.nl>
2018-12-06 18:39:29 +0100

    2.0.3 - Bugfixes; contrastkleuren in widget en reactieform verbeterd.

--- README.md
+++ README.md
@@ -13,9 +13,10 @@ Dit theme wordt gebruikt op [digitaleoverheid.nl](https://www.digitaleoverheid.n
 * eh
 
 ## Current version:
-2.0.2 - Searchform styling hersteld.
+2.0.3 - Bugfixes; contrastkleuren in widget en reactieform verbeterd.
 
 ## Version history
+* 2.0.3 - Bugfixes; contrastkleuren in widget en reactieform verbeterd.
 * 2.0.2 - Searchform styling hersteld.
 * 2.0.1 - Definities voor CPT verplaatst naar plugin.
 * 1.2.20 - Site-improve: alleen skiplink voor hoofdnavigatie gebruiken op homepage.
--- functions.php
+++ functions.php
@@ -8,8 +8,8 @@
 // * @author  Paul van Buuren
 // * @license GPL-2.0+
 // * @package wp-rijkshuisstijl
-// * @version 2.0.2
-// * @desc.   Searchform styling hersteld.
+// * @version 2.0.3
+// * @desc.   Bugfixes; contrastkleuren in widget en reactieform verbeterd.
 // * @link    https://github.com/ICTU/digitale-overheid-wordpress-theme-rijkshuisstijl
  */
 
@@ -23,8 +23,8 @@ include_once( get_template_directory() . '/lib/init.php' );
 // Constants
 define( 'CHILD_THEME_NAME',                 "Rijkshuisstijl (Digitale Overheid)" );
 define( 'CHILD_THEME_URL',                  "https://wbvb.nl/themes/wp-rijkshuisstijl" );
-define( 'CHILD_THEME_VERSION',              "2.0.2" );
-define( 'CHILD_THEME_VERSION_DESCRIPTION',  "Searchform styling hersteld." );
+define( 'CHILD_THEME_VERSION',              "2.0.3" );
+define( 'CHILD_THEME_VERSION_DESCRIPTION',  "Bugfixes; contrastkleuren in widget en reactieform verbeterd." );
 define( 'SHOW_CSS_DEBUG',                   false );
 //define( 'SHOW_CSS_DEBUG',                   true );
 
