75aa2ca40aad89d1b1c85091e5306824a05e384e
Paul van Buuren <paul@wbvb.nl>
2019-01-30 21:44:50 +0100

    2.2.5b - Kleursuggestie Pim voor donkerder oranje.

--- distribute.sh
+++ distribute.sh
@@ -1,6 +1,6 @@
 # sh /shared-paul-files/Webs/git-repos/ICTU---Digitale-Overheid-WP-theme/distribute.sh &>/dev/null
 
-# version 2.2.4c
+# version 2.2.5b
 
 # clear the log file
 
@@ -45,19 +45,21 @@ mv '/shared-paul-files/Webs/temp/nl_NL.mo' '/shared-paul-files/Webs/temp/wp-rijk
 # copy files to /wp-content/languages/themes
 rsync -ah '/shared-paul-files/Webs/temp/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/languages/themes/'
 
-# ------------------
+# --------------------------------------------------------------------------------------------------------------------------------
 echo "bekkuppie maken ---------------------------------------------"
+# --------------------------------------------------------------------------------------------------------------------------------
 
-rsync -r -a -v --delete '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/themes/wp-rijkshuisstijl/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/themes/wp-rijkshuisstijl-accept-2.2.4c/'
+rsync -r -a -v --delete '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/themes/wp-rijkshuisstijl/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/themes/wp-rijkshuisstijl-accept-2.2.5b/'
 
 # change the theme name
-sed -i '.bak' 's/Rijkshuisstijl (Digitale Overheid)/2.2.4c - Homepage: plaatje helemaal weggehaald./g' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/themes/wp-rijkshuisstijl-accept-2.2.4c/style.css'
+sed -i '.bak' 's/Rijkshuisstijl (Digitale Overheid)/2.2.5b - Kleursuggestie Pim voor donkerder oranje./g' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/themes/wp-rijkshuisstijl-accept-2.2.5b/style.css'
 
 # remove the backup
-rm '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/themes/wp-rijkshuisstijl-accept-2.2.4c/style.css.bak'
+rm '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/themes/wp-rijkshuisstijl-accept-2.2.5b/style.css.bak'
 
 
 
+# --------------------------------------------------------------------------------------------------------------------------------
 echo "Dutchlogic --------------------------------------------------"
 
 
@@ -72,18 +74,21 @@ rsync -ah '/shared-paul-files/Webs/temp/' '/shared-paul-files/Webs/ICTU/Gebruike
 
 
 
+# --------------------------------------------------------------------------------------------------------------------------------
 echo "DO single site ----------------------------------------------"
 
 # copy files to /wp-content/languages/themes
 rsync -ah '/shared-paul-files/Webs/temp/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/do-singlesite/wp-content/languages/themes/'
 rsync -r -a  --delete '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/live-dutchlogic/wp-content/themes/wp-rijkshuisstijl/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/do-singlesite/wp-content/themes/wp-rijkshuisstijl/'
 
+# --------------------------------------------------------------------------------------------------------------------------------
 echo "Sentia Accept -----------------------------------------------"
 
 # copy files to /wp-content/languages/themes
 rsync -ah '/shared-paul-files/Webs/temp/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/accept/www/wp-content/languages/themes/'
 rsync -r -a  --delete '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/live-dutchlogic/wp-content/themes/wp-rijkshuisstijl/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/accept/www/wp-content/themes/wp-rijkshuisstijl/'
 
+# --------------------------------------------------------------------------------------------------------------------------------
 echo "Sentia Live -------------------------------------------------"
 rsync -ah '/shared-paul-files/Webs/temp/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/live/www/wp-content/languages/themes/'
 rsync -r -a  --delete '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/live-dutchlogic/wp-content/themes/wp-rijkshuisstijl/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/live/www/wp-content/themes/wp-rijkshuisstijl/'
