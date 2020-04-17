8f225ffe06a792a3b6fb2210c621d6ad1ed5c557
Paul van Buuren <paul@wbvb.nl>
2020-04-17 12:01:18 +0200

    Menu verbouwd zodat bij schermbreedtewijzigingen

--- js/menu.js
+++ js/menu.js
@@ -8,29 +8,66 @@
 // * @license GPL-2.0+
 // * @package wp-rijkshuisstijl
 // * @version 2.14.2
-// * @desc.   Menu herzien voor mobiel schermbreedtes.
+// * @desc.   Menu verbouwd zodat bij schermbreedtewijzigingen wel of niet de mobiele styling gebruikt wordt
 // * @link    https://github.com/ICTU/digitale-overheid-wordpress-theme-rijkshuisstijl
 
 // Vars
-var header      = document.querySelector('#genesis-nav-primary'),
-    menu        = document.querySelector('nav .wrap ul.menu'),
-    menuButton  = document.querySelector('.menu-button');
-
+var header      	= document.querySelector('#genesis-nav-primary'),
+    menu        	= document.querySelector('nav .wrap ul.menu'),
+    menuButton  	= document.querySelector('.menu-button'),
+    menuwrapper 	= header.querySelector(".wrap"),
+    herosearchform	= document.getElementById("herosearchform"),
+    heroimage 		= document.querySelector('.hero-image .wrapper');
 
 // =========================================================================================================
 
 function hideMenuButton(document, window, undefined) {
-	
+
 	header.classList.remove('menu-met-js');
 	header.classList.remove('active');
 	header.classList.add('geen-menu-button');
 	menu.setAttribute('aria-hidden', 'false');
 	
-	var ele = document.getElementById("menu-button");
+	if ( ( typeof(menuwrapper) != 'undefined' && menuwrapper != null) )  {
 	
-	if (ele) {
-		// Remove button from page
-		header.removeChild(menuButton);
+		var div_homelink_button = document.getElementById("homelink_button");
+		
+		// search form terugplaatsen in hero-image
+		if ( ( typeof(herosearchform) != 'undefined' && herosearchform != null) )  {
+			heroimage.append(herosearchform);
+		}
+
+		if ( ( typeof(menu) != 'undefined' && menu != null) )  {
+			
+			var home_exists  = document.querySelector('li.home');
+			
+			if ( ( typeof(home_exists) != 'undefined' && home_exists != null) )  {
+				// listitem with class .home already exists
+				// no need to reinsert it
+			} else {
+
+				var lihome	= document.createElement('li');
+				lihome.setAttribute('class', 'home');
+				lihome.innerHTML 		= '<a href="/">Home</a>';
+
+				var theP = div_homelink_button.querySelector('p');
+
+				if (theP.classList.contains('is_home')) {
+
+					lihome.classList.add('current-menu-item');
+				}	
+				
+				menu.insertBefore(lihome, menu.childNodes[0]);
+			}
+		}
+
+		if ( ( typeof(div_homelink_button) != 'undefined' && div_homelink_button != null) )  {
+			// Remove button from page
+			menuwrapper.removeChild(div_homelink_button);
+		}
+		
+
+	
 	}
 }
 
@@ -40,33 +77,37 @@ function showMenuButton(document, window, undefined) {
 	
 	'use strict';
 
-	//Attempt to get the element using document.getElementById
-	var herosearchform		= document.getElementById("herosearchform");
-	var menuwrapper 		= header.querySelector(".wrap");
-	
 	if ( ( typeof(menuwrapper) != 'undefined' && menuwrapper != null) )  {
 
 		if ( ( typeof(herosearchform) != 'undefined' && herosearchform != null) )  {
 			menuwrapper.prepend(herosearchform);
 		}
 
-		var listitem_home_a = '<a href="/">Home</a>';
-		var divcontainer	= document.createElement('div');
-		var lihome 			= menuwrapper.querySelector("li.home");
-		
-		divcontainer.setAttribute('id', 'homelink_button');
-		menuwrapper.prepend( divcontainer );
-
-		if ( ( typeof(lihome) != 'undefined' && lihome != null) )  {
-			// er is een list-item met class home in de wrapper
-			listitem_home_a 		= lihome.innerHTML;
-			lihome.remove();
-		}
+		var listitem_home_a 	= '<a href="/">Home</a>';
+		var div_homelink_button	= document.createElement('div');
+		var lihome 				= menuwrapper.querySelector("li.home");
 
 		// Create a link to home
 		menuButton = document.createElement('p');
 		menuButton.innerHTML = listitem_home_a;
-		divcontainer.appendChild(menuButton);
+
+		
+		div_homelink_button.setAttribute('id', 'homelink_button');
+		menuwrapper.prepend( div_homelink_button );
+
+		if ( ( typeof(lihome) != 'undefined' && lihome != null) )  {
+			// er is een list-item met class home in de wrapper
+			listitem_home_a 		= lihome.innerHTML;
+
+			if (lihome.classList.contains('current-menu-item')) {
+				menuButton.classList.add('is_home');
+			}	
+
+			lihome.remove();
+		}
+
+
+		div_homelink_button.appendChild(menuButton);
 
 		// Create a button and set properties
 		menuButton = document.createElement('button');
@@ -76,7 +117,7 @@ function showMenuButton(document, window, undefined) {
 		menuButton.setAttribute('aria-expanded', 'false');
 		menuButton.setAttribute('aria-controls', 'menu');
 		menuButton.innerHTML = '<b>menu</b>';
-		divcontainer.appendChild(menuButton);
+		div_homelink_button.appendChild(menuButton);
 
 		if (header.classList.contains('init')) {
 
@@ -87,7 +128,12 @@ function showMenuButton(document, window, undefined) {
 			menuButton.setAttribute('aria-label', 'Open menu');
 			menuButton.setAttribute('aria-expanded', 'false');
 			
+		} else {
+
+			menu.setAttribute('aria-hidden', 'true');
+
 		}
+		
 	}
 
 	// Menu properties
@@ -128,33 +174,31 @@ function showMenuButton(document, window, undefined) {
 // media query change
 function WidthChange(mq) {
 
-	if ( mq.addListener ) {
-
-		if (mq.matches) {
-			// window width is at least 900px
-			// don't show menu button
-			hideMenuButton(document, window);
-		}
-		else {
-			// window width is less than 900px
-			// DO show menu button
-			showMenuButton(document, window);
-		}
+	if (mq.matches) {
+		// window width is at least 900px
+		// don't show menu button
+		hideMenuButton(document, window);
 	}
+	else {
+		// window width is less than 900px
+		// DO show menu button
+		showMenuButton(document, window);
+	}
+
 }
 
 // =========================================================================================================
+
+var mq = window.matchMedia('(min-width: 900px)');
 
 // media query event handler
-if (matchMedia) {
+//if (matchMedia) {
 	
-	var mq = window.matchMedia('(min-width: 900px)');
-	if ( mq.addListener ) {
+//	if ( mq.addListener ) {
 		mq.addListener( WidthChange );
-	}
+//	}
 	WidthChange(mq);
 
-}
+//}
 
 // =========================================================================================================
-
