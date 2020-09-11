a52c01e424de934833b362bffb70c91ed145ed08
Paul van Buuren <paul@wbvb.nl>
2020-09-11 11:37:51 +0200

    lalala versienummer voor live-server

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
  * @author  Paul van Buuren
  * @license GPL-2.0+
  * @package wp-rijkshuisstijl
- * @version 2.22.4
+ * @version 2.22.5
  * @desc.   Verdere styling Gravity Forms. Eerste opzet voor alert-bannerruimte.
  * @link    https://github.com/ICTU/digitale-overheid-wordpress-theme-rijkshuisstijl
  */
@@ -23,7 +23,7 @@ include_once( get_template_directory() . '/lib/init.php' );
 // Constants
 define( 'CHILD_THEME_NAME', "Rijkshuisstijl (Digitale Overheid)" );
 define( 'CHILD_THEME_URL', "https://wbvb.nl/themes/wp-rijkshuisstijl" );
-define( 'CHILD_THEME_VERSION', "2.22.4" );
+define( 'CHILD_THEME_VERSION', "2.22.5" );
 define( 'CHILD_THEME_VERSION_DESCRIPTION', "Verdere styling Gravity Forms. Eerste opzet voor alert-bannerruimte." );
 define( 'SHOW_CSS_DEBUG', false );
 //define( 'SHOW_CSS_DEBUG',                   true );
